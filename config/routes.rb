6a01aae7adda84d305cb785b13b106e16d6d5b25
melissadawn189@gmail.com <melissadawn189@gmail.com>
2017-01-30 11:16:07 -0500

    4.5 styling carousel

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,5 @@ Rails.application.routes.draw do
 
   root 'static_pages#landing_page', page: 'home'
 
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
