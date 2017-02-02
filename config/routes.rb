084c580faac6c37c1be4df5171952a28c7fe2ef8
melissadawn189@gmail.com <melissadawn189@gmail.com>
2017-02-01 21:20:42 -0500

    5.1 routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
-  root 'static_pages#landing_page', page: 'home'
+  get 'static_pages/landing_page'
+
+  root 'static_pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
