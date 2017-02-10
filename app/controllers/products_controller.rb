460edacdd43534dc5af48d18fc3b6571d8b4d695
melissadawn189@gmail.com <melissadawn189@gmail.com>
2017-02-10 17:26:06 -0500

    fixed products page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,17 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    if params[:q]
+      search_term = params[:q]
+        if Rails.env.production?
+          @products = Product.where("name ilike ?", "%#{search_term}%")
+          # return filtered list here
+        else
+          @products = Product.where("name LIKE ?", "%#{search_term}%")
+        end
+    else
+      @products = Product.all
+    end
   end
 
   # GET /products/1
