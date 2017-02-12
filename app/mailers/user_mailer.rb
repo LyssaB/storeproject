5f52e9075e4a3dc0435155cd3d148debe1302591
melissadawn189@gmail.com <melissadawn189@gmail.com>
2017-02-11 19:28:21 -0500

    5.3 fixing user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
 
 	def contact_form(email, name, message)
 	@message = message
-		mail(:from => "melissadawn189@gmail.com",
+		mail(:from => email,
 			:to => "lyssasstuff@gmail.com",
 			:subject => "You've got mail from #{name}")
 	end
