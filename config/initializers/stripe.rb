20a162e8ba5e4b4081c1ad3c6149f884fb5694c8
melissadawn189@gmail.com <melissadawn189@gmail.com>
2017-03-26 16:32:49 -0400

    Added email receipts

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -8,5 +8,6 @@ else
     :publishable_key => 'pk_live_pS0FYL14RPmmfrsnFoP4xbFX',
     :secret_key => 'sk_live_vdbgVsdMA2cQpxjO9sMniKq8'
   }
-
 end
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
