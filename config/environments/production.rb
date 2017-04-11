f0607adf9e06d1846b1f108c30c0238580c8b12f
melissadawn189@gmail.com <melissadawn189@gmail.com>
2017-04-10 21:23:44 -0400

    6.13 fixing typo in cable.yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,5 +93,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://mddesigns.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://mddesings.herokuapp.com', 'http://mddesigns.herokuapp.com']
 end
 
