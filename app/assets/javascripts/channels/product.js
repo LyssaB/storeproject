776d4f94bf2cc524d88435b74f142419a1805538
LyssaB <melissadawn189@gmail.com>
2017-04-15 11:34:03 -0400

    6.14 ActiveJob

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -10,6 +10,19 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
   received: function(data) {
     // Called when there's incoming data on the websocket for this channel
     $(".alert-info").show();
+    $(".prod_reviews").prepend(data.comment);
+    $("#average-rating").attr("data-score", data.average_rating);
+    refreshRating();
     setTimeout(function(){ $(".alert-info").hide(); }, 5000);
+  },
+
+  listen_to_comments: function() {
+    return this.perform("listen", {
+      product_id: $("[data-product-id]").data("product-id")
+    });
   }
+});
+
+$(document).on("turbolinks:load", function() {
+  App.product.listen_to_comments();
 });
