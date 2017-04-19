2cf8753e44504aaaffc328972f13059456ef131f
LyssaB <melissadawn189@gmail.com>
2017-04-19 14:27:38 -0400

    reverting payments_controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,30 +3,29 @@ class PaymentsController < ApplicationController
 
   def create
   token = params[:stripeToken]
-  @product = Product.find(params[:product_id])
-  @amount = @product.price #price in cents
+  @product = params[:product_id]
   @user = current_user
     begin
       charge = Stripe::Charge.create(
-        :amount => @amount # amount in cents, again
+        :amount => @product.price, # amount in cents, again
         :currency => "usd",
         :source => token,
         :description => params[:stripeEmail]
       )
 
       if charge.paid
-        Order.create!(
-          user_id: @user.id,
-          product_id: @product.id,
-          total: @product.price
+        Order.create(
+
+          user_id: @user_id,
+          product_id: @product_id,
+          total: @product_price
           )
       end
       rescue Stripe::CardError => e
       body = e.json_body
       err = body[:error]
-      flash[:error] = "I'm sorry, there was a problem processing your payment: #{err[:message]}"
-      redirect_to product_path(@product)
+      flash[:error] = "I'm sorry, there was a problem processeing your payment: #{err[:message]}"
     end
-    redirect_to product_path(@product), notice: "Thank you for your purchase!"
+
   end
 end
