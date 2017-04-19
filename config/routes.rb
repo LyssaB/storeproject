ac1f11957b3aa677c3eb3f4c37e6e72e7bd71002
LyssaB <melissadawn189@gmail.com>
2017-04-19 17:19:50 -0400

    add payments/create route for redirect

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,8 @@ Rails.application.routes.draw do
 
   post 'payments/create'
 
+  get 'payments/create'
+
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}, :controllers => {:registrations => "user_registration"}
 
   mount ActionCable.server => '/cable'
